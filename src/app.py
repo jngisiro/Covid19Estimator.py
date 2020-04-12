9dbe763000462ad5ee8105a1fbb9bd7ac1a51ede
Paul Ngisiro <jngisiro@gmail.com>
2020-04-12 17:14:37 +0300

    Fix: flask app

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ from json import loads
 from dicttoxml import dicttoxml
 from flask import request, jsonify, send_from_directory
 
-from estimator import estimator
+# from estimator import estimator
 
 app = flask.Flask(__name__)
 
@@ -14,8 +14,8 @@ handler = logging.handlers.RotatingFileHandler(
     "access.log", maxBytes=1024*1024)
 logging.getLogger("werkzeug").setLevel(logging.DEBUG)
 logging.getLogger("werkzeug").addHandler(handler)
-app.logger.setLevel(logging.WARNING)
-app.logger.addHandler(handler)
+# app.logger.setLevel(logging.WARNING)
+# app.logger.addHandler(handler)
 
 
 @app.route("/", methods=["GET"])
@@ -23,22 +23,22 @@ def index():
     return "<h2>Covid 19 Estimator</h2>"
 
 
-@app.route("/api/v1/on-covid-19", methods=["POST"])
-@app.route("/api/v1/on-covid-19/json", methods=["POST"])
-def make_estimate():
-    input_data = request.json
-    return jsonify(estimator(input_data))
+# @app.route("/api/v1/on-covid-19", methods=["POST"])
+# @app.route("/api/v1/on-covid-19/json", methods=["POST"])
+# def make_estimate():
+#     input_data = request.json
+#     return jsonify(estimator(input_data))
 
 
-@app.route("/api/v1/on-covid-19/xml", methods=["POST"])
-def make_estimate_json():
-    input_data = request.json
-    return dicttoxml(estimator(input_data))
+# @app.route("/api/v1/on-covid-19/xml", methods=["POST"])
+# def make_estimate_json():
+#     input_data = request.json
+#     return dicttoxml(estimator(input_data))
 
 
-@app.route("/api/v1/on-covid-19/logs", methods=["GET"])
-def get_logs():
-    return send_from_directory("./", "access.log")
+# @app.route("/api/v1/on-covid-19/logs", methods=["GET"])
+# def get_logs():
+#     return send_from_directory("./", "access.log")
 
 
 app.run()
